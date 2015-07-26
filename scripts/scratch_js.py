761ee9da83046ec97b67397c398242c8722c3d42
Phil B <phil@Phils-MacBook-Pro-2.local>
2015-07-26 17:44:05 +0100

    Added command line argument for choosing simulator or live models

--- scripts/scratch_js.py
+++ scripts/scratch_js.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python
-#This code just extends the existing mesh code for backwards compatibility
+# This code extends the existing mesh code to enable the new JavaScript scratch module to communicate with our robot/robot simulator.
+# Rather than immediately broadcasting sensor values when they change. These robot controllers cache the sensor values and wait until
+# a reporter code fraegment from scratch requests the sensor value. It is then returned back to the scratch JavaScript module.
 import argparse
 from websocket_server import WebsocketServer
 import scratch_simulator_background_script
 import multiprocessing
 import logging
+import scratch_rover_5_background_script
+import serial
 
 #Constants - should be shared amongst all python scripts
 PREPARE_FOR_COMMAND_CMD = "prepareforcommand"
+MODE_BLUETOOTH = 'bluetooth'
+MODE_SIMULATOR = 'simulator'
 RESET_CMD = "reset"
 DETECT_ARTIFACT_CMD = "detectartifact"
 MOVE_CMD = "move"
@@ -18,12 +24,37 @@ REPORTER_RESULT_CMD = "reporterResult"
 HOST_NAME = 'localhost'
 WEBSOCKETS_PORT_NUMBER = 42004
 
+BLUETOOTH_SERIAL_PORT = "/dev/rfcomm2"
+BLUETOOTH_SERIAL_BAUD_RATE = 9600
+
 global logging
-# Extends existing simulator - this should hopefully be useful for the robot controller too
+
+
+# Extends existing simulator to cache the sensor results
 class JSRobotSimulator( scratch_simulator_background_script.RobotSimulator ):
 
     def __init__( self, commandQueue, reporterValuesQueue,  completedCommandsQueue):
         scratch_simulator_background_script.RobotSimulator.__init__(self, None, commandQueue)
+        self.reporterValuesQueue = reporterValuesQueue
+        self.completedCommandsQueue = completedCommandsQueue
+
+    def sendSensorUpdate( self, reporterKey, value ):
+        logging.debug("Sensor update: %s, %s"%(reporterKey, str(value)))
+        #TODO - what happens if multiple commands are waiting from different scratch threads?
+        if reporterKey == ALL_CMDS_COMPLETE:
+            if value == 1:
+                self.completedCommandsQueue.put((reporterKey, str(value)))
+            #else ignore
+        else:
+            #else is a sensor/reporter value
+            self.reporterValuesQueue.put((reporterKey, value))
+
+
+# Extends existing controller to cache the sensor results
+class JSRobotController( scratch_rover_5_background_script.RobotController):
+
+    def __init__( self, commandQueue, reporterValuesQueue,  completedCommandsQueue, bluetoothSerial):
+        scratch_rover_5_background_script.RobotController.__init__(self, None, commandQueue, bluetoothSerial )
         self.reporterValuesQueue = reporterValuesQueue
         self.completedCommandsQueue = completedCommandsQueue
 
@@ -86,6 +117,25 @@ if __name__ == '__main__':
         help="Be verbose",
         action="store_const", dest="loglevel", const=logging.INFO,
     )
+
+    subparsers = parser.add_subparsers(dest='mode')
+    simulatorParser = subparsers.add_parser(MODE_SIMULATOR)
+    bluetoothParser = subparsers.add_parser(MODE_BLUETOOTH)
+    bluetoothParser.add_argument(
+        '-p', '--port',
+        help="Bluetooth serial port",
+        dest="port",
+        default=BLUETOOTH_SERIAL_PORT,
+        required=True,
+    )
+    bluetoothParser.add_argument(
+        '-b', '--baudrate',
+        help="Bluetooth baud rate",
+        dest="baudrate",
+        default=BLUETOOTH_SERIAL_BAUD_RATE,
+        type=int,
+    )
+
     args = parser.parse_args()
     logging.basicConfig(level=args.loglevel, format='%(asctime)s %(message)s')
 
@@ -101,7 +151,14 @@ if __name__ == '__main__':
     server.set_fn_client_left(client_left)
     server.set_fn_message_received(message_received)
 
-    httpRobotSimulator = JSRobotSimulator(commandQueue, reporterValuesQueue, completedCommandsQueue)
+    if args.mode == MODE_SIMULATOR:
+        httpRobotSimulator = JSRobotSimulator(commandQueue, reporterValuesQueue, completedCommandsQueue)
+    elif args.mode == MODE_BLUETOOTH:
+        bluetoothSerial = serial.Serial( args.port, args.baudrate, timeout=0 )
+        httpRobotSimulator = JSRobotController(commandQueue, reporterValuesQueue, completedCommandsQueue, bluetoothSerial)
+    else:
+        raise "Unrecognised mode"
+
     server.httpRobotSimulator = httpRobotSimulator
     httpRobotSimulator.start()
     server.timeout = 0.1 #allows us to check if any commands have completed
